22ac4beca3ab0724d264c0ff4406b07bd330934f
gabriel-arinez <gabrielarinez056@gmail.com>
2026-05-23 05:12:06 -0400

    Creación de schema GraphQL

--- services/medical-history/src/graphql/schema/index.js
+++ services/medical-history/src/graphql/schema/index.js
@@ -1,4 +1,35 @@
-// Esquemas GraphQL (esqueleto)
-// No implementar lógica todavía.
+const typeDefs = `#graphql
+  type ClinicalRecord {
+    id: ID!
+    patientId: String!
+    bloodType: String
+    allergies: [String!]
+    chronicDiseases: [String!]
+    medications: [String!]
+    notes: [String!]
+    createdAt: String
+    updatedAt: String
+  }
 
-module.exports = {};
+  input ClinicalRecordInput {
+    patientId: String!
+    bloodType: String
+    allergies: [String!]
+    chronicDiseases: [String!]
+    medications: [String!]
+    notes: [String!]
+  }
+
+  type Query {
+    healthGraphQL: String
+    patientRecords(patientId: String!): [ClinicalRecord!]!
+    record(id: ID!): ClinicalRecord
+  }
+
+  type Mutation {
+    createRecord(input: ClinicalRecordInput!): ClinicalRecord!
+    updateRecord(id: ID!, input: ClinicalRecordInput!): ClinicalRecord!
+  }
+`;
+
+export default typeDefs;
--- services/medical-history/src/index.js
+++ services/medical-history/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import typeDefs from "./graphql/schema/index.js";
 
 dotenv.config();
 
